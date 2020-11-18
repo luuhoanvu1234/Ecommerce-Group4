6ec74a57a22d2ba685e05507b345d3d4ac4becaa
Nguyễn Thanh Hưng <55803563+17130081NguyenThanhHung@users.noreply.github.com>
2020-11-18 14:12:54 +0700

    update
    edit config database

--- core/settings.php
+++ core/settings.php
@@ -1,12 +1,16 @@
 <?php
  
 # MySQL Host
-$sql_db_host = "localhost";  // mặc định là localhost
+$sql_db_host = "	cdm1s48crk8itlnr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";  // mặc định là localhost
+// $sql_db_host = "localhost"; 
 # MySQL Tài khoản
-$sql_db_user = "root";
+$sql_db_user = "j58s0e4u1aewr3ct";
+// $sql_db_user = "root";
 # MySQL Mật khẩu
-$sql_db_pass = "000000";
+$sql_db_pass = "ewfd3f6bo6ke4akf";
+// $sql_db_pass = "000000";
 # MySQL Data
-$sql_db_name = "sql-raovat";
+$sql_db_name = "jnporanf25yqoqa9";
+// $sql_db_name = "sql-raovat";
 #  Chỉnh sửa URL website của bạn
 $site_url    = "https://localhost/demotmdt/Web-raovat/"; // E.g (http://example.com)
